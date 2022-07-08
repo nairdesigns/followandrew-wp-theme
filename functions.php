9394344dfba8e75c7870cfeb6d2f4e114b91d90f
Nair Designs` <nairbdg@gmail.com>
2022-07-09 00:49:45 +0800

    added dynamic jquery php code

--- functions.php
+++ functions.php
@@ -1,12 +1,20 @@
 <?php
 function followandrew_register_styles(){
 
-    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/style.css", array('followandrew-bootstrap'), '1.0', 'all'); 
-    wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '1.0', 'all'); 
-    wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '1.0', 'all'); 
+    $version = wp_get_theme()->get( 'Version' );
+    wp_enqueue_style('followandrew-style', get_template_directory_uri() . "/style.css", array('followandrew-bootstrap'), $version , 'all'); 
+    wp_enqueue_style('followandrew-bootstrap', "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css", array(), '4.4.1', 'all'); 
+    wp_enqueue_style('followandrew-fontawesome', "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css", array(), '5.13.0', 'all'); 
 }
 
 add_action( 'wp_enqueue_scripts', 'followandrew_register_styles');
 
+function followandrew_register_scripts(){
+
+    wp_enqueue_script('followandrew-bootstrap','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),'3.4.1', );
+
+}
+
+add_action( 'wp_enqueue_scripts', 'followandrew_register_styles');
 
 ?>
