9b444d84db1390a6ffd2887243e4e33b084c43ca
Nair Designs` <nairbdg@gmail.com>
2022-07-11 15:20:47 +0800

    moved footer and header files to sepereate tags

--- functions.php
+++ functions.php
@@ -1,4 +1,12 @@
 <?php
+
+function followandrew_theme_support(){
+    //adds dynamic title tag support
+    add_theme_support( 'title_tag' );
+
+}
+
+add_action('after_setup_theme', 'followandrew_theme_support');
 function followandrew_register_styles(){
 
     $version = wp_get_theme()->get( 'Version' );
@@ -11,10 +19,12 @@ add_action( 'wp_enqueue_scripts', 'followandrew_register_styles');
 
 function followandrew_register_scripts(){
 
-    wp_enqueue_script('followandrew-bootstrap','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),'3.4.1', );
-
+    wp_enqueue_script('followandrew-jquery','https://code.jquery.com/jquery-3.4.1.slim.min.js', array(),'3.4.1', true );
+    wp_enqueue_script('followandrew-popper','https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js', array(),'3.4.1', true );
+    wp_enqueue_script('followandrew-bootstrap','https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js', array(),'3.4.1', true );
+    wp_enqueue_script('followandrew-main',get_template_directory_uri() . "/assets/js/main.js", array(),'1.0', true );
 }
 
-add_action( 'wp_enqueue_scripts', 'followandrew_register_styles');
+add_action( 'wp_enqueue_scripts', 'followandrew_register_scripts');
 
 ?>
